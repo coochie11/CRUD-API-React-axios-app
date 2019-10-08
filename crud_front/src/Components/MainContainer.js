02dc1e8941a8f84e366bf607fdbe8ea568d8b4f2
Youta <Youta@MacBook-Pro-4.local>
2019-10-08 16:11:24 +0900

    now we can make POST

--- crud_front/src/Components/MainContainer.js
+++ crud_front/src/Components/MainContainer.js
@@ -2,6 +2,8 @@ import React from 'react'
 import axios from 'axios'
 import ProductsContainer from './ProductsContainer'
 import FormContainer from './FormContainer'
+import update from 'react-addons-update'
+
 
 class MainContainer extends React.Component{
     constructor(props){
@@ -23,12 +25,25 @@ class MainContainer extends React.Component{
         })
     }
 
+    createProduct = (product) => {
+        axios.post('http://localhost:3001/products',{product: product})
+        .then((response) =>{
+            const newData = update(
+                this.state.products, {$push:[response.data]}
+            )
+            this.setState({products: newData})
+        })
+        .catch((data) =>{
+            console.log(data)
+        })
+    }
+
     render(){
         return(
             <div className="app-main">
                 <h1>Main Controller</h1>
 
-                <FormContainer />
+                <FormContainer createProduct={this.createProduct} />
                 <ProductsContainer productData={
                     this.state.products
                 } />
